06c1de98283fc1e974b429a8077505409ec3fdd4
Miguel Espinoza <obitosth@gmail.com>
2021-11-22 23:25:32 -0500

    react

---
+++ react-app/src/components/Grid/index.js
@@ -0,0 +1,17 @@
+function Grid(props) {
+  const { children, columns } = props;
+
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${columns},1fr)`,
+        gridGap: "20px",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export { Grid };
